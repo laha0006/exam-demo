7494ec4b2a36e75d5d118f985ed405a6c284e328
Lars <144362461+laha0006@users.noreply.github.com>
2025-01-09 06:02:08 +0100

    Initial commit

---
+++ src/main/java/dev/tolana/examdemo/ExamDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.tolana.examdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/tolana/examdemo/ExamDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tolana.examdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
